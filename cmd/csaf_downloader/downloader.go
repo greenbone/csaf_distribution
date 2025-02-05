b700519865ffe549eed6c1e6814aab303e43ebf0
mgoetzegb <marius.goetze@greenbone.net>
2025-02-05 14:06:15 +0100

    fix: fix memory leak on failed http requests (#59)
    In many error cases the http response body was not closed as the `defer`
    statement closing the body was only executed after the error handling.
    
    Thus the underlying connection can't be reused. This caused the creation
    of many goroutines which are never discarded, massively increasing the
    memory consumption.
    
    A prominent scenario is if a CSAF provider does not provide both sha256
    and sha512 checksums.
    
    ## References
    
    Jira Ticket: [VTI-405](https://jira.greenbone.net/browse/VTI-405)

--- cmd/csaf_downloader/downloader.go
+++ cmd/csaf_downloader/downloader.go
@@ -484,6 +484,16 @@ nextAdvisory:
 				"error", err)
 			continue
 		}
+		responseBody, err := io.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			stats.downloadFailed++
+			errorCh <- csafErrs.ErrNetwork{Message: fmt.Sprintf("can't read response body containing CSAF document %s from URL %s: %v", filename, file.URL(), err)}
+			slog.Warn("Cannot read response body",
+				"url", file.URL(),
+				"error", err)
+			continue
+		}
 
 		if resp.StatusCode != http.StatusOK {
 			switch {
@@ -548,8 +558,7 @@ nextAdvisory:
 		var doc any
 
 		if err := func() error {
-			defer resp.Body.Close()
-			tee := io.TeeReader(resp.Body, hasher)
+			tee := io.TeeReader(bytes.NewReader(responseBody), hasher)
 			return json.NewDecoder(tee).Decode(&doc)
 		}(); err != nil {
 			stats.downloadFailed++
@@ -766,11 +775,11 @@ func loadSignature(client util.Client, p string) (*crypto.PGPSignature, []byte, 
 	if err != nil {
 		return nil, nil, err
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		return nil, nil, fmt.Errorf(
 			"fetching signature from '%s' failed: %s (%d)", p, resp.Status, resp.StatusCode)
 	}
-	defer resp.Body.Close()
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
 		return nil, nil, err
@@ -787,11 +796,11 @@ func loadHash(client util.Client, p string) ([]byte, []byte, error) {
 	if err != nil {
 		return nil, nil, err
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode != http.StatusOK {
 		return nil, nil, fmt.Errorf(
 			"fetching hash from '%s' failed: %s (%d)", p, resp.Status, resp.StatusCode)
 	}
-	defer resp.Body.Close()
 	var data bytes.Buffer
 	tee := io.TeeReader(resp.Body, &data)
 	hash, err := util.HashFromReader(tee)
