118c8459959976557aa25e5e8ad34f5ae19d0d7d
Marius Goetze <marius.goetze@greenbone.net>
2025-07-04 11:01:32 +0200

    fix typo in changes.csv error handliing

--- csaf/advisories.go
+++ csaf/advisories.go
@@ -230,7 +230,7 @@ func (afp *AdvisoryFileProcessor) loadChanges(
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		switch { // we don't expect 401 and 403, as diretory based feeds are supposed to be public, but just to be on the safe side
+		switch { // we don't expect 401 and 403, as directory based feeds are supposed to be public, but just to be on the safe side
 		case resp.StatusCode == http.StatusUnauthorized:
 			return nil, errs.ErrInvalidCredentials{Message: fmt.Sprintf("invalid credentials for accessing %s: %s", changesURL, resp.Status)}
 		case resp.StatusCode == http.StatusForbidden:
